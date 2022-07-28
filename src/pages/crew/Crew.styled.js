2c5d7986acb0c1a83e8da91d4375f1a9980202dd
azepelayo <Aze.Pelayo@mrm.com>
2022-07-28 17:28:34 +0800

    Radio buttons update, prettify

--- src/pages/crew/Crew.styled.js
+++ src/pages/crew/Crew.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { colors } from "../../shared/styled-components/Shared.styled";
 
 export const CrewContainer = styled.div`
     display: grid;
@@ -15,10 +16,10 @@ export const CrewContainer = styled.div`
 
     @media screen and (min-width: 768px) {
         grid-template-areas:
-        "Heading5"
-        "CrewDetails"
-        "CrewRadioButtons"
-        "CrewImage";
+            "Heading5"
+            "CrewDetails"
+            "CrewRadioButtons"
+            "CrewImage";
         padding: 0 4em;
         gap: 1em 0;
     }
@@ -27,16 +28,16 @@ export const CrewContainer = styled.div`
         grid-template-columns: 1fr 1fr;
         grid-template-rows: auto;
         grid-template-areas:
-        "Heading5 CrewImage"
-        "CrewDetails CrewImage"
-        "CrewRadioButtons CrewImage";
+            "Heading5 CrewImage"
+            "CrewDetails CrewImage"
+            "CrewRadioButtons CrewImage";
         height: 84.5vh;
         justify-items: start;
         align-items: start;
-        padding:0 0 0 10vw;
+        padding: 0 0 0 10vw;
         gap: 1em 2em;
     }
-`
+`;
 
 export const CrewRadioButtons = styled.div`
     grid-area: CrewRadioButtons;
@@ -47,18 +48,42 @@ export const CrewRadioButtons = styled.div`
 
     @media screen and (min-width: 768px) {
         width: 20%;
-    };
+    }
 
     @media screen and (min-width: 1024px) {
         align-self: end;
         padding-bottom: 3em;
-    };
-`
+        width: 10em;
+    }
+`;
+
+export const RadioInput = styled.input`
+    appearance: none;
+    background-color: ${colors.white};
+    opacity: 0.17;
+    height: 1em;
+    width: 1em;
+    border-radius: 50%;
+    cursor: pointer;
+    border: none;
+
+    &:focus,
+    &:hover {
+        opacity: 0.5;
+        border: none;
+        outline: none;
+    }
+
+    &:checked,
+    &:active {
+        opacity: 1;
+    }
+`;
 
 export const CrewImage = styled.img`
     grid-area: CrewImage;
     width: auto;
-    border-bottom: 0.5px solid rgba(100,100,100, 0.5);
+    border-bottom: 0.5px solid rgba(100, 100, 100, 0.5);
     padding: 0 2.5em;
     height: 250px;
 
@@ -67,14 +92,14 @@ export const CrewImage = styled.img`
         bottom: 0;
         height: calc(100% - 555px);
         border: none;
-    };
+    }
 
     @media screen and (min-width: 1024px) {
         position: static;
         align-self: end;
         height: 85%;
     }
-`
+`;
 
 export const CrewDetails = styled.div`
     grid-area: CrewDetails;
@@ -86,9 +111,10 @@ export const CrewDetails = styled.div`
 
     @media screen and (min-width: 1024px) {
         text-align: left;
-        
+        width: 100%;
+
         p {
-            width: 70%;
+            width: 90%;
         }
     }
 `;
